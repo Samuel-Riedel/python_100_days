e7fee501e39ece78c9f7024de3691b67596e5ef0
samuel-riedel <samuel.riedel@outlook.com>
2024-05-13 05:39:11 +0200

    tip_calculator

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -1,3 +1,15 @@
 print("Welcome to the tip calculator")
 
-print("Hello"[4])
+bill = float(input("What was the total bill? \n"))
+
+tip = float(input("How much would you like to give? 10, 12, or 15? \n"))
+tip /= 100
+percentage = bill * tip
+
+split_bill = float(input("How many people to split the bill? \n"))
+
+subTotal = (bill + percentage) / split_bill
+total = round(subTotal,2)
+
+print(f"Each person should pay: {total} $")
+
