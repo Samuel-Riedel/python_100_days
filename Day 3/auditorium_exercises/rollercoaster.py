8d0a5e005481074b70c575c05ffc5c898e638ae8
samuel-riedel <samuel.riedel@outlook.com>
2024-06-22 06:21:45 +0200

    added_midlife_crisis_if_rollercoaster

--- Day 3/auditorium_exercises/rollercoaster.py
+++ Day 3/auditorium_exercises/rollercoaster.py
@@ -11,13 +11,19 @@ if height >= 120:
     elif age <= 17:
         bill = 7
         print("Youth tickets are 7$")
-    elif age >=18:
+    elif age >=18 and age <=44:
         bill = 12
         print("Adult tickets are 12$")
-    wants_photo = input("Do you want a photo taken? Y or N.\n").lower()
-    if wants_photo == "y":
-            bill += 3
-    print(f"The price of the photo is 3$, your total is: {bill}")
+    elif age >=45 and age <=55:
+        bill = 0
+        print(f"Midlife crisis tickets are 0$")
+    wants_photo = input("Do you want a photo taken? Y or N.\n").upper()
+    if wants_photo == "Y":
+        bill += 3
+        print(f"The price of the photo is 3$, your total is: {bill}")
+    else:
+        print(f"Your total bill is: {bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride")
     
