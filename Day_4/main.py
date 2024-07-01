246e3e4c1013331e61161c5f8f098d6a5413967a
samuel-riedel <samuel.riedel@outlook.com>
2024-07-02 01:42:24 +0200

    rockPaperScissor_done

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,22 +1,22 @@
 import random
 from rockPaperScissorsObjects import rock, paper,scissors
 
-position = input("What do you choose?\n Type 0 for Rock \n Type 1 for paper\n Type 2 for Scissors\nType a number: ")
+user = input("What do you choose?\n Type 0 for Rock \n Type 1 for paper\n Type 2 for Scissors\nType a number: ")
 list = [0,1,2] #Ignore still not sure if its going to be used
 objectList = [rock,paper,scissors]# this will be printed depending on the user input, its using index numbers
 
-number_index = int(position) #this code converts the input into an int
+number_index = int(user) #this code converts the input into an int
 show = objectList[number_index]# show uses the variable number_index to pick the user input and print the object from the list
 print(show)
-if position == "0":
+if user == "0":
     print("###################\nYou picked Rock\n###################")
-elif position == "1":
+elif user == "1":
     print("###################\nYou picked Paper\n###################")
-elif position == "2":
+elif user == "2":
     print("###################\nYou picked Scissors\n###################")
 
 
-randomNumber = random.randint(0,1)
+randomNumber = random.randint(0,2)
 print(randomNumber)
 if randomNumber == 0:
     print("###################\nBot picked Rock\n###################")
@@ -27,10 +27,24 @@ elif randomNumber == 2:
 showBot = objectList[randomNumber]# show uses the variable number_index to pick the user input and print the object from the list
 print(showBot)
 
-if position == "0" and showBot ==0:
+if user == "0" and randomNumber ==0:
     print("Its a tie!")
-elif position == "1" and randomNumber ==1:
+elif user == "1" and randomNumber ==1:
     print("Its a tie!")
-elif position == "2" and showBot ==2:
+elif user == "2" and randomNumber ==2:
     print("Its a tie!")
 
+if user  == "0" and randomNumber == 1:
+    print("Bot won!")
+elif user == "0" and randomNumber == 2:
+    print("You won!")
+
+if user  == "1" and randomNumber == 0:
+    print("Bot won!")
+elif user == "1" and randomNumber == 2:
+    print("You won!")
+
+if user  == "2" and randomNumber == 0:
+    print("Bot won!")
+elif user == "2" and randomNumber == 1:
+    print("You won!")
--- main.py
+++
@@ -1,6 +0,0 @@
-from botOutput import botOutput
-from userInput import userInput
-
-userInput()
-
-botOutput()
