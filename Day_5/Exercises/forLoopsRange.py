03189456e38ba9b15bbf1d1623dfc23cbb527ce9
samuel-riedel <samuel.riedel@outlook.com>
2024-07-03 05:40:32 +0200

    addinEvenNumbers_auditorium

--- Day_5/Exercises/forLoopsRange.py
+++ Day_5/Exercises/forLoopsRange.py
@@ -1,4 +1,8 @@
-total = 5051
-for number in range(1,101):
+target=int(input("Number goes here: "))
+
+target = target +2
+
+total = 0
+for number in range(0,target,2):
     total += number
 print(total)
