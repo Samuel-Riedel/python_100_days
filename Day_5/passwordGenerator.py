10d6ea6d36f9909b3ae5bc025048b61ff950118b
Samuel-Riedel <samuel.riedel@outlook.com>
2024-07-04 11:32:42 +0200

    random_shuffle

--- Day_5/passwordGenerator.py
+++ Day_5/passwordGenerator.py
@@ -5,6 +5,7 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+question = input("Would you like to scramble your random generated password? Type Y/N").lower()
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
@@ -15,18 +16,33 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level - Order of characters randomized:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+sum = nr_letters + nr_numbers + nr_symbols
 
-randomizerLetters= str(random.sample(letters,nr_letters))
-randomizerNumbers= str(random.sample(numbers,nr_numbers))
-randomizerSymbols= str(random.sample(symbols,nr_symbols))
+randomizerLetters= random.sample(letters,nr_letters)
+randomizerNumbers= random.sample(numbers,nr_numbers)
+randomizerSymbols= random.sample(symbols,nr_symbols)
+
 
 storage = randomizerLetters,randomizerNumbers, randomizerSymbols
-for i in storage:
-    print(i, end='')
-print(f"This is your random genrated password: {str(storage)}")
-print(str("".join(storage)))
-print(*storage)
-print("".join(map(str, storage)))
+#shuffleList = random.shuffle(storage)
+
+if question =="n":
+        for i in storage:
+            for x in i:
+                randomGenerator = print(x, end ="")
+else:
+        for i in storage:
+            for j in storage:
+             randomGenerator = print(list(j), end ="")
+             print(type(randomGenerator))
+             shuffleGenerator = list(j)
+             print(type(shuffleGenerator))
+
+
+               
+
+
+
 
 
 
