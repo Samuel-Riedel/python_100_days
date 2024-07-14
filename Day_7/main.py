bbb99067c32b66524a653004f64b4bc248193b75
samuel-riedel <samuel.riedel@outlook.com>
2024-07-14 04:09:56 +0200

    step_2_1

--- Day_7/main.py
+++ Day_7/main.py
@@ -1,15 +1,12 @@
 import random
 
 words = ["hola","adios","comida"]
+display = []
 
 guess = input("Please select one letter from the alphabet:\n").lower()
-print("Arriba de esto va esta la letra que yo hice input no se esta impriminedo")
-randomizeWord = random.sample(words,1) # this selects a random word from the list called words this is step 2
-chosen_word = random.choice(words)
-#this appends the selected word to a new variable called chosen word this is step 1
-"""for word in chosenLetter:
-    guess.append(word)
-"""
+chosen_word = random.choice(words) # this selects a random word from the list called words this is step 2
+
+
 #-------------------------------------------------------------------------#
 
 for letter in chosen_word:
@@ -19,33 +16,10 @@ for letter in chosen_word:
         print("Wrong")
 
 
+for x in range(len(chosen_word)):
+    display.append("_")
 
-"""for items in characterList:
-    newItems = chosenLetter
-    if newItems == characterList:
-        print("y")
-    else:
-        print("w")"""
+print(display)
     
-"""for i in randomizeWord:
-    for x in i:
-        print(x)
-        new.append(x)
-"""
 
 #-------------------------------------------------------------------------#
-"""print(randomizeWord)
-print(guess)
-
-print(type(randomizeWord))
-print(type(guess))"""
-
-
-"""for x in i:
-        print(x)
-        if x == i:
-            print("YES")
-        else:
-            print("NOOOO")
-"""
-
