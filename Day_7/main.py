e8e3d2a0dff611faefc696c5ea73eeecd544e882
samuel-riedel <samuel.riedel@outlook.com>
2024-07-14 03:45:31 +0200

    first_part_done_Hangman

--- Day_7/main.py
+++ Day_7/main.py
@@ -2,25 +2,30 @@ import random
 
 words = ["hola","adios","comida"]
 
-chosenLetter = input("Please select one letter from the alphabet:\n").lower()
+guess = input("Please select one letter from the alphabet:\n").lower()
 print("Arriba de esto va esta la letra que yo hice input no se esta impriminedo")
 randomizeWord = random.sample(words,1) # this selects a random word from the list called words this is step 2
-guess = [] #This stores the selected letter from the user
-
-characterList = list(randomizeWord[0])
-
+chosen_word = random.choice(words)
 #this appends the selected word to a new variable called chosen word this is step 1
-for word in chosenLetter:
+"""for word in chosenLetter:
     guess.append(word)
-
+"""
 #-------------------------------------------------------------------------#
 
-for items in characterList:
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+
+"""for items in characterList:
     newItems = chosenLetter
     if newItems == characterList:
         print("y")
     else:
-        print("w")
+        print("w")"""
     
 """for i in randomizeWord:
     for x in i:
