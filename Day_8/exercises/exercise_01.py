b31a749c41608497b922e1b15d313c90a1c36163
samuel-riedel <samuel.riedel@outlook.com>
2024-07-22 04:05:36 +0200

    greeting_with_name_and_location

--- Day_8/exercises/exercise_01.py
+++ Day_8/exercises/exercise_01.py
@@ -1,6 +1,11 @@
-def test():
+def greet():
     print("Hello")
     print("Hello")
     print("Hello")
 
-test()
+greet()
+
+def greet_with(name, location):
+    print(f"Hello, Welcome back {name}! You need to go to {location} to deliver the food")
+
+greet_with("Samuel","Manchester")
