c2b98e8c761a84e44fcab65706b818778412e987
samuel-riedel <samuel.riedel@outlook.com>
2024-07-24 02:50:35 +0200

    fixed_print_on_def_encrypt_used_join

--- Day_8/main.py
+++ Day_8/main.py
@@ -3,25 +3,20 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+encoded_word = []
 
 
 def encrypt(text,shift):
     for i in text:
         index_text = alphabet.index(i)
         index_text += shift 
-        print(alphabet[index_text])
+        encoded_word.append(alphabet[index_text])
+        #print(f"The encoded message is: {alphabet[index_text]}")
+    print("".join(encoded_word))
+    print("".join(encoded_word))
+
         
 
-
-
-
-
-
-
-
-
-
 encrypt(text,shift)
-
 
 
