588492b6f822a2a513794b1fc4ef39f48dee90f1
samuel-riedel <samuel.riedel@outlook.com>
2024-08-20 01:41:53 +0200

    finished_bidding_system

--- Day_09/main.py
+++ Day_09/main.py
@@ -8,20 +8,21 @@ while answer == "yes":
     name = input("What is your name?\n")
     bid = int(input("How much are you going to bid?\n"))
     answer = input("yes or no \n")
+    dictionary[name] = bid
+
 
 
     if answer == "no":
 
         print("The Highest Bidder is:\n")
-        #keyValue = max(dictionary, key=dictionary.get)
-        for key, value in dictionary.items():
-            #print(keyValue, dictionary[keyValue])
-            print(key,":", value)
+        highest_bidder = max(dictionary, key=dictionary.get)
+        highest_bid = dictionary[highest_bidder]
+    
+        print(f"\nThe Highest Bidder is {highest_bidder} with a bid of ${highest_bid}")
         answer = "no"
 
     elif answer == "yes":
-        dictionary["name"] = name
-        dictionary["bid"] = bid
+        pass
 
     
 
