091e244b0aa8f6f436ed9cb107b0bccba7148fc5
samuel-riedel <samuel.riedel@outlook.com>
2024-09-02 03:29:17 +0200

    basic_logic_done_for_normal_difficulty

--- Day_12/numberGuessing.py
+++ Day_12/numberGuessing.py
@@ -6,14 +6,29 @@ LIFES = 10
 RANDOMNUMBER = random.randint(1,100)
 DIFFICULTY = input("Which difficulty you want to play? Normal/Hard")
 
+print(f"CHEATIING TIME {RANDOMNUMBER}")
+
 
 
 #This is the function normal
 def normal():
+    global LIFES
     print("You have selected the normal difficulty")
-    print(f"You have {LIFES} lifes left!")
 
-    #while LIFES >0:
+#code will keep executing until user has 0 lifes
+while LIFES > 0:
+    userNumber = int(input("Please guess a number from 1 to 100"))
+    if userNumber == RANDOMNUMBER: 
+        print("you guessed the right number!")
+        print("GAME OVER!")
+        break
+    if userNumber != RANDOMNUMBER:
+        print("You guessed the wrong number, try again!")
+        LIFES = LIFES -1
+        print(f"You have {LIFES} lifes left!")
+
+
+
         
 
 
