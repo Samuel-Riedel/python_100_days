e3b550c097ada791ccf2da60d0c86997b9d2c969
samuel-riedel <samuel.riedel@outlook.com>
2024-09-02 05:23:45 +0200

    finished_number_guessing_game

--- Day_12/numberGuessing.py
+++ Day_12/numberGuessing.py
@@ -1,10 +1,11 @@
 import random
+from art import gameOver
 
 print("Welcome to the guessing number game!")
 
 LIFES = 10
 RANDOMNUMBER = random.randint(1,100)
-DIFFICULTY = input("Which difficulty you want to play? Normal/Hard")
+DIFFICULTY = input("Which difficulty you want to play? Normal/Hard\n").lower()
 
 #This is the function normal
 def normal():
@@ -13,7 +14,7 @@ def normal():
 
 #code will keep executing until user has 0 lifes
     while LIFES > 0:
-        userNumber = int(input("Please guess a number from 1 to 100"))
+        userNumber = int(input("Please guess a number from 1 to 100\n"))
         if userNumber == RANDOMNUMBER: 
             print("you guessed the right number!")
             print("GAME OVER!")
@@ -26,17 +27,20 @@ def normal():
                 print("Your number is too high!")
             elif userNumber <  RANDOMNUMBER:
                 print("Your number is too low!")
+            elif LIFES == 0:
+                gameOver()
+                print(f"The secret number was: {RANDOMNUMBER}")
 
 #This is the function hard
 def hard():
     global LIFES
     LIFES = 5
-    print("You have selected the hard difficulty")
+    print("You have selected the hard difficulty\n")
 
     while LIFES > 0:
-        userNumber = int(input("Please guess a number from 1 to 100"))
+        userNumber = int(input("Please guess a number from 1 to 100\n" ))
         if userNumber == RANDOMNUMBER: 
-            print("you guessed the right number!")
+            print(f"you guessed the right number! {RANDOMNUMBER}")
             print("GAME OVER!")
             break
         if userNumber != RANDOMNUMBER:
@@ -47,6 +51,9 @@ def hard():
                 print("Your number is too high!")
             elif userNumber <  RANDOMNUMBER:
                 print("Your number is too low!")
+            if LIFES == 0:
+                gameOver()
+                print(f"The secret number was: {RANDOMNUMBER}")
 
 
 
@@ -57,4 +64,3 @@ if DIFFICULTY == "normal":
 else:
     hard()
 
-print(f"CHEATING TIME {RANDOMNUMBER}") # place holder
