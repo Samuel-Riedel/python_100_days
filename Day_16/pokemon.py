e47a559ef34842af90740e827954453b03e827cd
samuel-riedel <samuel.riedel@outlook.com>
2024-09-22 06:29:29 +0200

    finished_day_16_but_still_need_to_redo_it

--- Day_16/pokemon.py
+++ Day_16/pokemon.py
@@ -1,11 +1,13 @@
 from prettytable import PrettyTable
 table = PrettyTable()
+table.align = "l"
 
-table.field_names = ["Pokemon","Type"]
+
+table.field_names = ["Pokemon","Type","Number"]
 table.add_rows([
-    ["Pikachu","Electric"],
-    ["Charizard","Fire"],
-    ["Gengar", "Ghost"]
+    ["Pikachu","Electric","xxx"],
+    ["Charizard","Fire","xxx"],
+    ["Gengar", "Ghost","xxx"]
     ])
 
 print(table)
