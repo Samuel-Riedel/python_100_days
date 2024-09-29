910597b56ba00d3ac26432e25d375dbd60eeb2d1
samuel-riedel <samuel.riedel@outlook.com>
2024-09-29 12:43:18 +0200

    fixed_floats_to_display_only_two_decimals

--- projects/promedio_calculator/main.py
+++ projects/promedio_calculator/main.py
@@ -7,22 +7,19 @@ total =0
 power = True
 
 while power == True:
-    user_number = input("Please type a value to create an average \n" ) # this takes the users input number and will stop executing if you type "stop"
+    user_number = input("Please type a value to create an average or type stop to finish calculation\n" ) # this takes the users input number and will stop executing if you type "stop"
     number_list.append(user_number) #This sends the user_number that was typed to number_list
 
     if user_number == "stop": #when stopped the program will turn of the variable called power to false, delete the word stop from the lis and calculate the average
         number_list.pop() # this deletes the stop msg you input
 
-        for i in number_list: # this is adding all the number from the list to the variable called average
-            average += int(i)
+        #for i in number_list: 
+            
 
-        for i in number_list: # this divides the number the items in the list top create an average
+        for i in number_list: # for loop divides the number of items in the list top create an average
+            average += float(i)# this variable  is adding all the number from the list to the variable called average
             total = average / len(number_list)
         
-        print(f"Your average is: {total}")
+        print(f"Your average is: {round(total,2)}")
 
-
-        power = False # turn off the while loop 
-
-
-    print(number_list)
+        power = False # turn off the while loop
